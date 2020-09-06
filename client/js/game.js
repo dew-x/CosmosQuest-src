c7b9bf1c8346346f843dfdfcd73da398b8a837cd
Matthieu Bonne <matthieu.bonne@gmail.com>
2020-09-06 14:17:29 +0200

    tooltip

--- client/js/game.js
+++ client/js/game.js
@@ -23200,7 +23200,7 @@ function Game() {
             }
         } else if (skill.type=="acum") {
             return {
-                short: "Aura/n+"+Math.floor(lvl/(1/value)).toFixed(0)+" Attack per "+skill.value2+" turns per "+skill.target+" levels",
+                short: "Aura/n+"+Math.floor(lvl/(1/value)).toFixed(0)+" Attack ("+Math.floor(lvl/(skill.target/value)).toFixed(0)+") per "+skill.value2+" turns per "+skill.target+" levels",
                 long: "Attack increases the amount of direct damage a hero deals, by the value. This value increases every number of turns displayed. The value of the attack increases based on the level of this unit. Auras affect all friendly units until the unit with the aura dies.",
             }
         } else if (skill.type=="empow") {
