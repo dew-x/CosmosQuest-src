64973fa5ee983448340182c17a5b4929627f1144
Simoris <carmot-gc@web.de>
2020-09-10 22:13:18 +0200

    Update version number
    Mostly to avoid getting "New version available, refresh" while testing
    new stuff ^^

--- client/js/game.js
+++ client/js/game.js
@@ -7,7 +7,7 @@ function Game() {
     var THERO = computeTHERO(THEROtid);
     var TPROMO = computeTPROMO(THEROtid);
     var DIMENSION = getDimensions();
-    var VERSION = "v4.8.4.1";
+    var VERSION = "v4.8.4.2";
     var _this = this;
     var data = undefined;
     var production = 0;
