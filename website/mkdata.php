e0ec1026b81ece2861502b2e02b87ec6b0f9d7f2
Matthieu Bonne <matthieu.bonne@gmail.com>
2020-09-23 15:08:20 +0200

    preparing 4.8.5

--- website/mkdata.php
+++ website/mkdata.php
@@ -49,7 +49,7 @@
     $limit=1600;
     if ($isSuper) $limit=1200;
     $data=array(
-        "version"=>"v4.8.4.2",
+        "version"=>"v4.8.5.0",
         "tournament"=>array(
             "pool"=>$pool
         ),
